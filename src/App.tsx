bc338e4dca7085923d57fb1b3237fda91f20cd11
Jimmy Larsson <jimmy.larsson@europe.com>
2023-09-22 11:34:01 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <p>velkommen till städsiddan</p>
+            <p>vi kan städa vi</p>
+        </>
+    )
+}
+
+export default App
