d806b451664535b55d62d65803c09541a122970f
nati schmidt <nati.schmidt97@outlook.de>
2023-09-27 12:04:44 +0200

    adjusted design WIP

--- src/reusableComponents/footer.tsx
+++ src/reusableComponents/footer.tsx
@@ -4,10 +4,10 @@ export default function Footer() {
     return (
         <div className="footer-container" style={styles.footer}>
             <div className="footer-logo">
-                <img src="" alt="Logo" style={styles.logo}/>
+                <img src="" alt="Logo" style={styles.logo} />
             </div>
             <div className="footer-social-icons" style={styles.socialIcon}>
-                <a href="" >
+                <a href="">
                     <div className="social-facebok"></div>
                 </a>
                 <a href="">
@@ -17,35 +17,36 @@ export default function Footer() {
                     <div className="social-instagram"></div>
                 </a>
             </div>
-            <div className="footer-categories"  style={styles.category}>
-                <div className="footer-category">
+            <div className="footer-categories" style={styles.categoriesContainer}>
+                <div className="footer-category" style={styles.category}>
                     <h3>Om oss</h3>
                     <ul style={styles.subCategory}>
-                        <li> 1</li>
-                        <li> 2</li>
-                        <li> 3</li>
+                        <li>1</li>
+                        <li>2</li>
+                        <li>3</li>
                     </ul>
                 </div>
-                <div className="footer-category">
+                <div className="footer-category" style={styles.category}>
                     <h3>Kontakt</h3>
                     <ul style={styles.subCategory}>
                         <li>FAQ</li>
-                        <li> </li>
-                        <li> </li>
+                        <li></li>
+                        <li></li>
                     </ul>
                 </div>
-                <div className="footer-category">
+                <div className="footer-category" style={styles.category}>
                     <h3>Profil</h3>
                     <ul style={styles.subCategory}>
                         <li>Mina sidor</li>
-                        <li> 2</li>
-                        <li> 3</li>
+                        <li>2</li>
+                        <li>3</li>
                     </ul>
                 </div>
             </div>
         </div>
     );
 }
+
 const styles = {
     footer: {
         // backgroundColor: '#333',
@@ -62,6 +63,9 @@ const styles = {
         fontSize: '24px',
         margin: '0 10px',
         color: '#fff',
+    },
+    categoriesContainer: {
+        display: 'flex',
     },
     category: {
         flex: '1',
