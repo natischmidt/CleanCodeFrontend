75b32ff71b80d1adbf06ba632b80a15e91a0b0dc
ansta12 <anton.stahlgren@ecutbildning>
2023-09-27 10:43:23 +0200

    Uppdatering: - Lagt till en mapp där vi ska lägga till komponenter som skall kunna återanvändas - Skapat och börjat koda i en header komponent
    Anton 27/9

---
+++ src/reusableComponents/header.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function header() {
+    return (
+        <div className="headerContainer">
+            <div className="logo">
+                <button></button>
+            </div>
+        </div>
+    )
+}
