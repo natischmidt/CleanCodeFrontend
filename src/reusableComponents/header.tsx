69d2b2c1011175182e32ef8911a5963ee745c122
ansta12 <anton.stahlgren@ecutbildning>
2023-09-27 14:16:30 +0200

    Uppdatering: - Lagt till en Bild på loggan, knappar för olika val med lätt design - Lagt in olika logor att välja på och importerat dessa
    Anton 27/9

--- src/reusableComponents/header.tsx
+++ src/reusableComponents/header.tsx
@@ -1,11 +1,21 @@
 import './App.css'
+import logo from "../assets/logo2.png";
 
 export default function header() {
     return (
-        <div className="headerContainer">
-            <div className="logo">
-                <button></button>
+        <>
+            <div className="headerContainer">
+                <div className="logo">
+                    <img id="logo3" src={logo} alt="logo3"/>
+                </div>
+                <div className="menuButtons">
+                    <button id="Booking">Booking</button>
+                    <button id="Employees">Employees</button>
+                    <button id="Customers">Customers</button>
+                    <button id="GDPR">GDPR</button>
+                    <button id="SignOut">SignOut</button>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
