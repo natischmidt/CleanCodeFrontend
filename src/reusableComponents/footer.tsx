c502fad45c5cccbed01cca7bcc9a6a99e0af5dfa
nati schmidt <nati.schmidt97@outlook.de>
2023-09-27 11:15:46 +0200

    added the footer, filled with everything from the design we mad wip

--- src/reusableComponents/footer.tsx
+++ src/reusableComponents/footer.tsx
@@ -1,9 +1,48 @@
 import React from 'react';
 
-
-export default function footer() {
+export default function Footer() {
     return (
-        <div className="Container">
+        <div className="footer-container">
+            <div className="footer-logo">
+                <img src="" alt="Logo" />
+            </div>
+            <div className="footer-social-icons">
+                <a href="">
+                    <div className="social-facebok"></div>
+                </a>
+                <a href="">
+                    <div className="social-twitter"></div>
+                </a>
+                <a href="">
+                    <div className="social-instagram"></div>
+                </a>
+            </div>
+            <div className="footer-categories">
+                <div className="footer-category">
+                    <h3>Om oss</h3>
+                    <ul>
+                        <li> 1</li>
+                        <li> 2</li>
+                        <li> 3</li>
+                    </ul>
+                </div>
+                <div className="footer-category">
+                    <h3>Kontakt</h3>
+                    <ul>
+                        <li>FAQ</li>
+                        <li> </li>
+                        <li> </li>
+                    </ul>
+                </div>
+                <div className="footer-category">
+                    <h3>Profil</h3>
+                    <ul>
+                        <li>Mina sidor</li>
+                        <li> 2</li>
+                        <li> 3</li>
+                    </ul>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
