52572182c01bb096bbf21e2c44100f3b323f54c4
ansta12 <anton.stahlgren@ecutbildning>
2023-09-27 15:06:37 +0200

    Uppdatering: - Förflyttat all design/css till styles i header komponenten istället för App.css
    Anton 27/9

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Footer from './reusableComponents/footer'
+import Header from "./reusableComponents/header";
 
 function App() {
 
     return (
         <>
+            <Header></Header>
             <div className="container">
 
             </div>
--- src/reusableComponents/header.tsx
+++ src/reusableComponents/header.tsx
@@ -1,21 +1,78 @@
-import './App.css'
-import logo from "../assets/logo2.png";
+import logo from "../assets/logo3.png";
 
-export default function header() {
+export default function Header() {
     return (
-        <>
-            <div className="headerContainer">
-                <div className="logo">
-                    <img id="logo3" src={logo} alt="logo3"/>
-                </div>
-                <div className="menuButtons">
-                    <button id="Booking">Booking</button>
-                    <button id="Employees">Employees</button>
-                    <button id="Customers">Customers</button>
-                    <button id="GDPR">GDPR</button>
-                    <button id="SignOut">SignOut</button>
-                </div>
+        <div className="headerContainer" style={styles.header}>
+            <div className="logo">
+                <img id="logo3" src={logo} alt="logo3" style={styles.logo}/>
             </div>
-        </>
+            <div className="menuButtons" style={styles.menuButtons}>
+                <button id="Booking" style={styles.booking}>Booking</button>
+                <button id="Employees" style={styles.employees}>Employees</button>
+                <button id="Customers" style={styles.customers}>Customers</button>
+                <button id="GDPR" style={styles.gdpr}>GDPR</button>
+                <button id="SignOut" style={styles.signout}>Sign Out</button>
+            </div>
+        </div>
     )
+}
+
+const styles = {
+    header: {
+        backgroundColor: '#E2FFF8',
+        borderBottom: '5px solid #52af66',
+        display: 'flex',
+        padding: '20px',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    logo: {
+        width: '320px',
+        height: '180px',
+    },
+    menuButtons: {
+        display: 'flex',
+        marginLeft: '45%',
+        color: '#000001',
+    },
+    booking: {
+        backgroundColor: '#53b067',
+        marginLeft: '5%',
+        width: '180px',
+        height: '60px',
+        fontSize: '1.2rem',
+        fontWeight: 'normal',
+    },
+    employees: {
+        backgroundColor: '#53b067',
+        marginLeft: '5%',
+        width: '180px',
+        height: '60px',
+        fontSize: '1.2rem',
+        fontWeight: 'normal',
+    },
+    customers: {
+        backgroundColor: '#53b067',
+        marginLeft: '5%',
+        width: '180px',
+        height: '60px',
+        fontSize: '1.2rem',
+        fontWeight: 'normal',
+    },
+    gdpr: {
+        backgroundColor: '#53b067',
+        marginLeft: '5%',
+        width: '180px',
+        height: '60px',
+        fontSize: '1.2rem',
+        fontWeight: 'normal',
+    },
+    signout: {
+        backgroundColor: '#53b067',
+        marginLeft: '5%',
+        width: '180px',
+        height: '60px',
+        fontSize: '1.2rem',
+        fontWeight: 'normal',
+    },
 }
