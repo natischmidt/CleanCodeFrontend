9c009ad0e88f37db4975b5f2cc394269f1c61756
nati schmidt <nati.schmidt97@outlook.de>
2023-09-27 11:26:53 +0200

    footer design  WIP added some placeholder styling and the rest of the stuff from our design template

--- src/reusableComponents/footer.tsx
+++ src/reusableComponents/footer.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <div className="footer-container">
+        <div className="footer-container" style={styles.footer}>
             <div className="footer-logo">
-                <img src="" alt="Logo" />
+                <img src="" alt="Logo" style={styles.logo}/>
             </div>
-            <div className="footer-social-icons">
-                <a href="">
+            <div className="footer-social-icons" style={styles.socialIcon}>
+                <a href="" >
                     <div className="social-facebok"></div>
                 </a>
                 <a href="">
@@ -17,10 +17,10 @@ export default function Footer() {
                     <div className="social-instagram"></div>
                 </a>
             </div>
-            <div className="footer-categories">
+            <div className="footer-categories"  style={styles.category}>
                 <div className="footer-category">
                     <h3>Om oss</h3>
-                    <ul>
+                    <ul style={styles.subCategory}>
                         <li> 1</li>
                         <li> 2</li>
                         <li> 3</li>
@@ -28,7 +28,7 @@ export default function Footer() {
                 </div>
                 <div className="footer-category">
                     <h3>Kontakt</h3>
-                    <ul>
+                    <ul style={styles.subCategory}>
                         <li>FAQ</li>
                         <li> </li>
                         <li> </li>
@@ -36,7 +36,7 @@ export default function Footer() {
                 </div>
                 <div className="footer-category">
                     <h3>Profil</h3>
-                    <ul>
+                    <ul style={styles.subCategory}>
                         <li>Mina sidor</li>
                         <li> 2</li>
                         <li> 3</li>
@@ -46,3 +46,29 @@ export default function Footer() {
         </div>
     );
 }
+const styles = {
+    footer: {
+        // backgroundColor: '#333',
+        color: '#fff',
+        padding: '20px',
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    logo: {
+        maxWidth: '100px',
+    },
+    socialIcon: {
+        fontSize: '24px',
+        margin: '0 10px',
+        color: '#fff',
+    },
+    category: {
+        flex: '1',
+        margin: '0 10px',
+    },
+    subCategory: {
+        listStyle: 'none',
+        padding: '0',
+    },
+};
