7506d9f1f563268c0cead826158a4a1e2f9d6236
nati schmidt <nati.schmidt97@outlook.de>
2023-09-29 13:58:03 +0200

    footer now fixed to the bottom of the page, redid design too match footer by anton

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,7 +1,8 @@
 import Header from "../reusableComponents/header";
+import Footer from "../reusableComponents/footer";
 
 export default function HomePage() {
     return (
-        <Header/>
+        <><Header/><Footer/></>
     )
 }
