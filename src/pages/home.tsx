a778489bcc39af8204052aa340bfd9e8d428310a
nati schmidt <nati.schmidt97@outlook.de>
2023-10-06 12:53:33 +0200

    added dashboard to admins homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,12 @@
 import Header from "../reusableComponents/header";
 import Footer from "../reusableComponents/footer";
+import {AdminDashboard} from "../components/AdminDashboard";
 
 export default function HomePage() {
     return (
         <>
             <Header/>
+            <AdminDashboard/>
             <Footer/>
         </>
     )
