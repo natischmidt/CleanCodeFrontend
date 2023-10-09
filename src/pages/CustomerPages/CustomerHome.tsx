b40d8eff4a3c5acc547b42dc1cc59270c1f90664
nati schmidt <nati.schmidt97@outlook.de>
2023-10-09 23:24:35 +0200

    added my pages/dashboard component

--- src/pages/CustomerPages/CustomerHome.tsx
+++ src/pages/CustomerPages/CustomerHome.tsx
@@ -1,14 +1,25 @@
 import Header from "../../reusableComponents/header";
 import Footer from "../../reusableComponents/footer";
 import Dashboard from "../../reusableComponents/dashboard";
+import React from "react";
 
 
-export default function CustomerHomePage() {
+export const  CustomerHomePage : React.FC = () => {
+
+    const testUserData = {
+        firstname: 'Testkund',
+        lastname: 'Test',
+        email: 'test@example.com',
+        password: 'test',
+        address: '123 Test',
+        SSnumber: '123456789',
+        phoneNumber: '555-555-5555',
+    };
     return (
         <>
             <Header/>
             CUSTOMER PAGE
-            {/*<Dashboard userType="customer" userData={ } />*/}
+            <Dashboard userType="customer" userData={ testUserData} />
             <Footer/>
         </>
     )
