ab86e25106fa3c779f493a01138f018ba955366a
nati schmidt <nati.schmidt97@outlook.de>
2023-10-10 10:08:56 +0200

    different styling depending on usertype WIP

--- src/pages/CustomerPages/CustomerHome.tsx
+++ src/pages/CustomerPages/CustomerHome.tsx
@@ -1,10 +1,13 @@
 import Header from "../../reusableComponents/header";
 import Footer from "../../reusableComponents/footer";
 import Dashboard from "../../reusableComponents/dashboard";
-import React from "react";
+import { adminStyles, customerStyles, employeeStyles} from "../../styles/styles";
+import React, {useState} from "react";
 
 
-export const  CustomerHomePage : React.FC = () => {
+
+export const CustomerHomePage: React.FC = () => {
+    const [userType, setUserType] = useState("customer");
 
     const testUserData = {
         firstname: 'Testkund',
@@ -15,12 +18,34 @@ export const  CustomerHomePage : React.FC = () => {
         SSnumber: '123456789',
         phoneNumber: '555-555-5555',
     };
+
+    let pageStyles: React.CSSProperties;
+
+    //this can be replaced with a context to check the usertype instead
+    if (userType === "admin") {
+        pageStyles = adminStyles;
+    } else if (userType === "user") {
+        pageStyles = employeeStyles;
+    } else {
+        pageStyles = customerStyles;
+    }
+
     return (
         <>
-            <Header/>
-            CUSTOMER PAGE
-            <Dashboard userType="customer" userData={ testUserData} />
-            <Footer/>
+            <div className="custom-style-customer" style={pageStyles}>
+            <Header />
+                CUSTOMER PAGE
+                <Dashboard userType="customer" userData={testUserData} />
+            <Footer />
+            </div>
         </>
-    )
-}
+    );
+};
+// const customerStyles: React.CSSProperties = {
+//     backgroundColor: "#f0f0f0",
+//     padding: "20px",
+//     border: "1px solid #ccc",
+//     borderRadius: "5px",
+//     boxShadow: "0 0 5px rgba(0, 0, 0, 0.2)",
+//     textAlign: "center",
+// };
---
+++ src/styles/styles.tsx
@@ -0,0 +1,17 @@
+export const adminStyles: React.CSSProperties = {
+    backgroundColor: '#89b6ac',
+};
+
+export const employeeStyles: React.CSSProperties = {
+    backgroundColor: '#2115d2',
+
+};
+
+export const customerStyles: React.CSSProperties = {
+    backgroundColor: "#a60303",
+    padding: "20px",
+    border: "1px solid #ccc",
+    borderRadius: "5px",
+    boxShadow: "0 0 5px rgba(0, 0, 0, 0.2)",
+    textAlign: "center",
+};
