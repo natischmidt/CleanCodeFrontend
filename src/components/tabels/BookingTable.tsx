8a51bb814adb111cf16869587ac23b47f4131afa
nati schmidt <nati.schmidt97@outlook.de>
2023-10-10 11:30:09 +0200

    merged the bookigntables

--- src/components/tabels/BookingTable.tsx
+++ src/components/tabels/BookingTable.tsx
@@ -2,21 +2,31 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Table from "../../reusableComponents/table";
 
+interface bookingTableProps {
+    onUpdate: (jobId : number) => void;
+}
 
-//denna används inte, bookingTable2 som gäller
-export const BookingTable: React.FC = () => {
-    const [bookingData, setBookingData] = useState<any[]>([]);
+export const BookingTable: React.FC <bookingTableProps> = ({ onUpdate }) => {
+    const [deleted, setDeleted] = useState(0);
+    const [customerData, setCustomerData] = useState<any[]>([]);
 
     useEffect(() => {
 
         axios.get('http://localhost:8080/api/jobs/getAllJobs')
             .then((response) => {
-                setBookingData(response.data);
+                //nånting så att man ser datumet läsbart
+                const formattedData = response.data.map((job: any) => {
+                    const date = new Date(job.date);
+                    const formattedDate = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+                    return { ...job, date: formattedDate, id : job.jobId };
+                });
+                setCustomerData(formattedData);
+                console.log(response.data)
             })
             .catch((error) => {
-                console.error('Error fetching booking data:', error);
+                console.error('Error fetching customer data:', error);
             });
-    }, []);
+    }, [deleted]);
 
     const columns = [
         { key: 'jobId', title: 'Booking ID' },
@@ -29,11 +39,35 @@ export const BookingTable: React.FC = () => {
         { key: 'customerId', title: 'Customer Id' },
     ];
 
+    const handleDelete = async (jobId: number) => {
+
+        try {
+            const Url = `http://localhost:8080/api/jobs/deleteJob`;
+            const headers = {
+                'jobId' : jobId?.toString()
+            }
+            const response = await axios.delete(Url, {headers});
+            console.log('Deleting job was successful', response.data);
+            setDeleted(x => x +1)
+        } catch (error) {
+            console.error('Error deleting job', error);
+        }
+    };
+
+    const handleUpdate =  (jobId: number) => {
+        onUpdate(jobId);
+    };
+
     return (
         <div className="booking-table" style={styles.bookingTable}>
-            <Table columns={columns} data={bookingData}  onDelete={ () => {}} onUpdate={ () => {}}/>
+            <Table
+                columns={columns}
+                data={customerData}
+                onDelete={handleDelete}
+                onUpdate={handleUpdate}
+            />;
         </div>
-    );
+    )
 };
 
 const styles = {
