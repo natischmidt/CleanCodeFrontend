cef89d0cca582af319bf47efb342c58b11bd199a
nati schmidt <nati.schmidt97@outlook.de>
2023-10-11 11:57:44 +0200

    customer homepage design WIP

--- src/pages/CustomerPages/CustomerHome.tsx
+++ src/pages/CustomerPages/CustomerHome.tsx
@@ -3,13 +3,28 @@ import {customerStyles} from "../../styles/styles";
 import React from "react";
 import {useUserType} from "../../components/UserTypeContext";
 import CustomerHeader from "../../components/CustomerComponents/CustomerHeader";
-import image from "../assets/customer-homepage-picture.png";
+import image from '/src/assets/customer-homepage-picture.png';
+import { CSSProperties } from 'react';
 
-const styles = {
+
+
+const styles: {
+    container: CSSProperties;
+    halfContainer: CSSProperties;
+    imageSection: CSSProperties;
+    // bookingSection: CSSProperties;
+    whiteSection: CSSProperties;
+    textContainer: CSSProperties;
+    headerText: CSSProperties;
+    subText: CSSProperties;
+} = {
     container: {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
+        justifyContent: 'center',
+        marginTop : "5%",
+        boxShadow: '0 0 5px rgba(0, 0, 0, 0.5)'
     },
     halfContainer: {
         width: '100%',
@@ -18,18 +33,40 @@ const styles = {
     },
     imageSection: {
         flex: 1,
-        background: `url(${image})`,
-        backgroundSize: 'cover',
+        backgroundImage: `url(${image})`,
+        backgroundSize: '676px 738px',
+        backgroundPosition: 'center',
+        margin: '0 2px',
     },
-    bookingSection: {
+    whiteSection: {
         flex: 1,
         background: 'white',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
     },
+    textContainer: {
+        marginTop :'100px',
+        textAlign: 'center',
+        color : 'black',
+
+    },
+    headerText: {
+        fontSize: '50px',
+        fontWeight: 'bold',
+        margin: '0',
+    },
+    subText: {
+        fontSize: '25px',
+    },
+    // bookingSection: {
+    //     flex: 1,
+    //     background: 'white',
+    //     display: 'flex',
+    //     flexDirection: 'column',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    // },
+
 };
+
 
 export const CustomerHomePage: React.FC = () => {
     const { userType } = useUserType();
@@ -42,10 +79,17 @@ export const CustomerHomePage: React.FC = () => {
             <div style={{ ...selectedStyles }}>
                 <div style={styles.container}>
                     <div style={styles.halfContainer}>
-                        <div style={styles.imageSection}></div>
-                        <div style={styles.bookingSection}>
-                            <BookingComponent />
+                        <div style={styles.whiteSection}>
+                            <div style={styles.textContainer}>
+                                <h1 style={styles.headerText}>Städafint AB magiskt rent</h1>
+                                <p style={styles.subText}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </div>
                         </div>
+                        <div style={styles.imageSection}></div>
+
+                        {/*<div style={styles.bookingSection}>*/}
+                        {/*    <BookingComponent />*/}
+                        {/*</div>*/}
                     </div>
                 </div>
             </div>
