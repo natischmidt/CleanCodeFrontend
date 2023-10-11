f9568fd00eadcfd64df3433576c37effd6e3a3b6
nati schmidt <nati.schmidt97@outlook.de>
2023-10-11 11:25:37 +0200

    customer homepage design WIP

--- src/pages/CustomerPages/CustomerHome.tsx
+++ src/pages/CustomerPages/CustomerHome.tsx
@@ -3,18 +3,51 @@ import {customerStyles} from "../../styles/styles";
 import React from "react";
 import {useUserType} from "../../components/UserTypeContext";
 import CustomerHeader from "../../components/CustomerComponents/CustomerHeader";
+import image from "../assets/customer-homepage-picture.png";
+
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    halfContainer: {
+        width: '100%',
+        display: 'flex',
+        height: '50vh',
+    },
+    imageSection: {
+        flex: 1,
+        background: `url(${image})`,
+        backgroundSize: 'cover',
+    },
+    bookingSection: {
+        flex: 1,
+        background: 'white',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+};
 
 export const CustomerHomePage: React.FC = () => {
     const { userType } = useUserType();
     const selectedStyles =
         userType === "Customer" ? customerStyles : {};
 
-
     return (
         <>
-          <CustomerHeader />
+            <CustomerHeader />
             <div style={{ ...selectedStyles }}>
-
+                <div style={styles.container}>
+                    <div style={styles.halfContainer}>
+                        <div style={styles.imageSection}></div>
+                        <div style={styles.bookingSection}>
+                            <BookingComponent />
+                        </div>
+                    </div>
+                </div>
             </div>
             <CustomerFooter />
         </>
