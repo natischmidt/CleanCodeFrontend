fd4909e3719c338ece7b44d8702934d0c6999203
Segtm <Segtmednymail@gmail.com>
2023-11-01 10:01:30 +0100

    https

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as fs from "fs";
 
 // https://vitejs.dev/config/
+const httpsOptions = {
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem'),
+  passphrase: 'test'
+};
 export default defineConfig({
+
   plugins: [react()],
+  server: {
+    https: httpsOptions,
+    open: true
+  },
 })
