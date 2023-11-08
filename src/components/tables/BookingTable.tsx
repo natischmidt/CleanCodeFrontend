e9f37cb6a0412f3a3b2141c0c05307be46a5efe3
mnell <mtothenell@gmail.com>
2023-11-08 16:32:02 +0100

    - Fixat med mer filter på bookings (anställda) /Ola

--- src/components/tables/BookingTable.tsx
+++ src/components/tables/BookingTable.tsx
@@ -14,6 +14,8 @@ const BookingTable: React.FC<bookingTableProps> = ({onUpdate/*, onKlarna*/}) => 
     const [deleted, setDeleted] = useState(0);
     const [customerData, setCustomerData] = useState<any[]>([]);
     const [myFilter, setMyFilter] = useState('');
+    const [status, setStatus] = useState('');
+    const [searchDate, setSearchDate] = useState('');
 
     useEffect(() => {
         const fetchData = async () => {
@@ -51,7 +53,8 @@ const BookingTable: React.FC<bookingTableProps> = ({onUpdate/*, onKlarna*/}) => 
 
 
     const filteredCustomerData = customerData.filter((customer) =>
-        (myFilter === '' || customer.jobtype === myFilter)
+        (myFilter === '' || customer.jobtype === myFilter) && (status === '' || customer.jobStatus === status) &&
+        (customer.date.includes(searchDate) || searchDate === '')
     )
 
     return (
@@ -68,6 +71,30 @@ const BookingTable: React.FC<bookingTableProps> = ({onUpdate/*, onKlarna*/}) => 
                         <option value="ADVANCED">Advanced</option>
                         <option value="DIAMOND">Diamond</option>
                         <option value="WINDOW">Window</option>
+                    </select>
+                </div>
+                <div>
+                    Search by date:
+                    <input
+                        typeof="text"
+                        value={searchDate}
+                        style={{width: '6rem', marginLeft: '0.5rem', height: '0.8rem'}}
+                        onChange={(e) => setSearchDate(e.target.value)}/>
+                </div>
+                <div>
+                    Filter by status
+                    <select
+                        value={status}
+                        onChange={(e) => setStatus(e.target.value)}
+                        style={{marginLeft: '0.5rem'}}>
+                        <option value="">All</option>
+                        <option value="PENDING">Pending</option>
+                        <option value="DONE">Done</option>
+                        <option value="APPROVED">Approved</option>
+                        <option value="UNAPPROVED">Unapproved</option>
+                        <option value="PROCESSING">Processing</option>
+                        <option value="PAID">Paid</option>
+                        <option value="CANCELLED">Cancelled</option>
                     </select>
                 </div>
             </div>
@@ -107,7 +134,8 @@ const styles = {
     filterContainer: {
         display: 'flex',
         justifyContent: 'center',
-        marginBottom: '1.5rem'
+        marginBottom: '1.5rem',
+        gridGap: '2rem',
     },
     container: {
         display: 'flex',
